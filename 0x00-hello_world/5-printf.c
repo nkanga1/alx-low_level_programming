266121c6f3515c0c27b479a6765a39e20c1511db
nkanga1 <edunkanga@gmail.com>
2022-12-09 06:15:41 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print out thr proper programming for me
+ *
+ * Return: return 0;
+ */
+int main(void)
+{
+printf("with proper programming, but the outcomr is a piece of art");
+return (0);
+
+}
